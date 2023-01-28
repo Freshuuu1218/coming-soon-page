09953847eb7e1f1d9f31ca5c0f2fa44da175dc49
Kacper Klimczak <kacper_klimczak_2609@protonmail.com>
2023-01-28 17:56:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('input');
+const submit = document.querySelector('label');
+const validate = document.querySelector('.validate')
+submit.addEventListener('click', ()=>{
+    function checkEmail(input){
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1-3}\.[0-9]{1-3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())){
+    // console.log('git')
+    alert('You are on our newsletter list now.');
+  }
+  else{
+    // console.log('nie git')
+    input.style.border = '2px solid hsl(0, 93%, 68%)';
+    let error = document.createElement('p');
+    error.innerText = window.innerWidth;
+    validate.appendChild(error);
+  }
+}
+
+
+checkEmail(input);
+})
+
