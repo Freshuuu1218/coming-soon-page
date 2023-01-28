6935a98775706e1871387d65520231efe0db9b9d
Kacper Klimczak <kacper_klimczak_2609@protonmail.com>
2023-01-28 20:50:53 +0100

    fix validation script

--- script.js
+++ script.js
@@ -2,20 +2,27 @@ const input = document.querySelector('input');
 const submit = document.querySelector('label');
 const validate = document.querySelector('.validate')
 submit.addEventListener('click', ()=>{
-    function checkEmail(input){
+    
+function checkEmail(input){
+    
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1-3}\.[0-9]{1-3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (re.test(input.value.trim())){
-    // console.log('git')
-    alert('You are on our newsletter list now.');
-  }
-  else{
-    // console.log('nie git')
-    input.style.border = '2px solid hsl(0, 93%, 68%)';
-    let error = document.createElement('p');
-    error.innerText = window.innerWidth;
-    validate.appendChild(error);
-  }
-}
+  
+        if (re.test(input.value.trim())){
+            // console.log('git')
+            alert('You are on our newsletter list now.');
+        }else{
+            // console.log('nie git')
+            input.style.border = '2px solid hsl(0, 93%, 68%)';
+            let error = document.createElement('p');
+            error.innerText = "This email address is not valid. Try again";
+            validate.appendChild(error);        
+            
+            setTimeout(()=>{
+                error.remove();
+            },4000)
+        }
+
+    }
 
 
 checkEmail(input);
